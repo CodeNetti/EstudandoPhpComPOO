5f67eb854f580ad839025b2f3bbbfbd677163ef9
CodeNetti <raphasimonettti@gmail.com>
2024-03-11 10:53:08 -0300

    Adiconando Métodos

---
+++ Banco/Piloto/src/Funcionario.php
@@ -0,0 +1,38 @@
+<?php
+
+class Funcionario extends Pessoa
+{
+private string $cargo;
+
+    public function __construct(string $nome, CPF $cpf, string $cargo)
+    {
+        parent::__construct($nome, $cpf);
+        $this->cargo = $cargo;
+    }
+
+    public function validaNomeFuncionario(string $nomeTitular)
+    {
+        if (strlen($nomeTitular) < 5) {
+            echo "Nome precisa ter pelo menos 5 caracteres";
+            exit();
+        }
+    }
+
+
+
+    public function getNome(): string
+    {
+        return $this->cargo;
+    }
+    public function setNome(string $nome): void
+    {
+        $this-> validaNome($this->nome);
+        $this->cargo = $nome;
+    }
+
+
+
+
+
+}
+
